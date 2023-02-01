9e7a4453a392176ff1cb48f6b95bcbaf892e660b
Karl George <karlgdev@gmail.com>
2023-02-01 12:12:44 +0100

    Rendering pagination component

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -10,7 +10,43 @@ interface Props {
 const Pagination = ({ numberOfPages, currentPage, setCurrentPage }: Props) => {
   const pageNumbers = [...Array(numberOfPages + 1).keys()].slice(1);
 
-  return <div>Pagination</div>;
+  const pageNumberList = pageNumbers.map((pageNumber) => (
+    <li key={pageNumber}>
+      <a href='#' onClick={() => setCurrentPage(pageNumber)}>
+        {pageNumber}
+      </a>
+    </li>
+  ));
+
+  const prevPage = (): void => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = (): void => {
+    if (currentPage !== numberOfPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href='#' onClick={prevPage}>
+            Previous
+          </a>
+        </li>
+        {pageNumberList}
+        <li>
+          <a href='#' onClick={nextPage}>
+            Next
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Pagination;
